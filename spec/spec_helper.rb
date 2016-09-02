791c38e3cd497e3585ec33f74f750b75a7371c4d
Adam Mueller <adam@stembolt.com>
2016-09-02 09:43:13 -0700

    Clean-up rspec configuration a tiny bit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,54 +50,29 @@ VCR.configure do |c|
 end
 
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-
-  # Infer an example group's spec type from the file location.
   config.infer_spec_type_from_file_location!
-
-  # == URL Helpers
-  #
-  # Allows access to Spree's routes in specs:
-  #
-  # visit spree.admin_path
-  # current_path.should eql(spree.products_path)
-  config.include Spree::TestingSupport::UrlHelpers
-
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
   config.mock_with :rspec
-  config.color = true
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # Capybara javascript drivers require transactional fixtures set to false, and we use DatabaseCleaner
-  # to cleanup after each test instead.  Without transactional fixtures set to false the records created
-  # to setup a test will be unavailable to the browser, which runs under a separate server instance.
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
   config.use_transactional_fixtures = false
 
-  # Ensure Suite is set to use transactions for speed.
+  config.fail_fast = ENV['FAIL_FAST'] || false
+
+  config.include FactoryGirl::Syntax::Methods
+  config.include Spree::TestingSupport::UrlHelpers
+
   config.before :suite do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with :truncation
   end
 
-  # Before each spec check if it is a Javascript test and switch between using database transactions or not where necessary.
   config.before :each do
     DatabaseCleaner.strategy = RSpec.current_example.metadata[:js] ? :truncation : :transaction
     DatabaseCleaner.start
   end
 
-  # After each spec clean the database.
   config.after :each do
     DatabaseCleaner.clean
   end
-
-  config.fail_fast = ENV['FAIL_FAST'] || false
-  config.order = 'random'
 end
