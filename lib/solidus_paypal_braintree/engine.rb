c86cbfdf5d753d087540a116257fc616fdc76a09
Adam Mueller <adam@stembolt.com>
2016-09-07 11:47:00 -0700

    Register gateway with Solidus
    Otherwise, admins can't select this as a payment method.

--- lib/solidus_paypal_braintree/engine.rb
+++ lib/solidus_paypal_braintree/engine.rb
@@ -9,6 +9,10 @@ module SolidusPaypalBraintree
       g.test_framework :rspec
     end
 
+    initializer "register_solidus_paypal_braintree_gateway", after: "spree.register.payment_methods" do |app|
+      app.config.spree.payment_methods << SolidusPaypalBraintree::Gateway
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
