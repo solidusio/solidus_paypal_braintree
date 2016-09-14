001b1e33262d3d49e211d7dbece819b30a3cbb82
Luuk Veenis <luuk@stembolt.com>
2016-09-14 14:06:07 -0700

    Add validations to TransactionAddress

--- app/models/solidus_paypal_braintree/transaction_address.rb
+++ app/models/solidus_paypal_braintree/transaction_address.rb
@@ -6,5 +6,8 @@ module SolidusPaypalBraintree
 
     attr_accessor :country_code, :last_name, :first_name,
       :city, :zip, :state_code, :address_line_1, :address_line_2
+
+    validates :first_name, :last_name, :address_line_1, :city, :zip,
+      :state_code, :country_code, presence: true
   end
 end
