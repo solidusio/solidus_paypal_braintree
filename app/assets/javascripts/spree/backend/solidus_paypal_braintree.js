61cd77e7041f23a5ff226ddbead7d15f647d2191
Andrew Stewart <andrew@stwrt.ca>
2016-09-14 16:48:29 -0700

    Add Braintree hosted fields inputs as source_form
    This commit provides some pseudo-inputs to the source form. These inputs
    are then enabled by braintree's Hosted Fields JS.
    
    A further commit will hook into the form submit to create a nonce and
    submit it to the backend.

--- app/assets/javascripts/spree/backend/solidus_paypal_braintree.js
+++ app/assets/javascripts/spree/backend/solidus_paypal_braintree.js
@@ -1,2 +1,98 @@
-// Placeholder manifest file.
-// the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
+//= require vendor/braintree/client.js
+//= require vendor/braintree/hosted_fields.js
+
+$(function() {
+  var $paymentForm = $("#new_payment"),
+      $hostedFields = $("[data-braintree-hosted-fields]");
+
+  // exit early if we're not looking at the New Payment form, or if no
+  // SolidusPaypalBraintree payment methods have been configured.
+  if (!$paymentForm.length || !$hostedFields.length) { return; }
+
+  function promisify(fn, args, self) {
+    var d = $.Deferred();
+
+    fn.apply(self || this, (args || []).concat(function (err, data) {
+      err && d.reject(err);
+      d.resolve(data);
+    }));
+
+    return d.promise();
+  }
+
+  function getToken(paymentMethodId, callback) {
+    var opts = {
+      url: "/solidus_paypal_braintree/client_token",
+      method: "POST",
+      data: {
+        payment_method_id: paymentMethodId
+      },
+    };
+
+    function onSuccess(data) {
+      return data.client_token;
+    }
+
+    return Spree.ajax(opts).then(onSuccess);
+  }
+
+  function createClient(token) {
+    var opts = { authorization: token };
+    return promisify(braintree.client.create, [opts]);
+  }
+
+  function createHostedFields(id) {
+    return function(client) {
+      var opts = {
+        client: client,
+
+        fields: {
+          number: {
+            selector: "#card_number" + id
+          },
+
+          cvv: {
+            selector: "#card_code" + id
+          },
+
+          expirationDate: {
+            selector: "#card_expiry" + id
+          }
+        }
+      };
+
+      return promisify(braintree.hostedFields.create, [opts]);
+    }
+  }
+
+  $hostedFields.each(function() {
+    var $this = $(this),
+        $new = $("[name=card]", $this);
+
+    var id = $this.data("id");
+
+    var hostedFieldsInstance;
+
+    $new.on("change", function() {
+      var isNew = $(this).val() === "new";
+
+      function onError(err) {
+        var msg = err.name + ": " + err.message;
+        show_flash("error", msg);
+        console.error(err);
+      }
+
+      function setHostedFieldsInstance(instance) {
+        hostedFieldsInstance = instance;
+      }
+
+      if (isNew && hostedFieldsInstance == null) {
+        getToken(id).
+          then(createClient).
+          then(createHostedFields(id)).
+          then(setHostedFieldsInstance).
+          catch(onError)
+      }
+    });
+  });
+});
