a5ef9eec13351cedcdbcefdd79028acb0843f777
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2016-10-17 11:24:34 -0700

    Support multiple merchant account ids by currency
    Braintree handles different currencies by using different merchant
    account id's to submit transactions. This allows a map in the
    preferences to keep which currencies should use which merchant accounts.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -56,7 +56,10 @@ module BraintreeHelpers
         environment: 'sandbox',
         public_key:  'mwjkkxwcp32ckhnf',
         private_key: 'a9298f43b30c699db3072cc4a00f7f49',
-        merchant_id: '7rdg92j7bm7fk5h3'
+        merchant_id: '7rdg92j7bm7fk5h3',
+        merchant_currency_map: {
+          'EUR' => 'stembolt_EUR'
+        }
       }
     }.merge(opts))
   end
