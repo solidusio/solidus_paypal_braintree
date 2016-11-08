0c29e3c33ed1d59a7186d559cc9bd2d4b9f5e999
Luuk Veenis <luuk@stembolt.com>
2016-11-07 17:05:45 -0800

    Use helper function to build transaction params

--- app/assets/javascripts/spree/frontend/solidus_paypal_braintree_frontend.js
+++ app/assets/javascripts/spree/frontend/solidus_paypal_braintree_frontend.js
@@ -39,48 +39,56 @@ onTokenizeCallback = function(tokenizeErr, payload) {
     if (tokenizeErr.type !== 'CUSTOMER') {
       console.error('Error tokenizing:', tokenizeErr);
     }
-    return;
   } else {
-    if (payload.details.shippingAddress.recipientName) {
-      var first_name = payload.details.shippingAddress.recipientName.split(" ")[0];
-      var last_name = payload.details.shippingAddress.recipientName.split(" ")[1];
-    }
-    if (first_name == null || last_name == null) {
-      var first_name = payload.details.firstName;
-      var last_name = payload.details.lastName;
-    }
-    var transactionParams = {
-      "payment_method_id" : window.paymentMethodId,
-      "transaction" : {
-        "email" : payload.details.email,
-        "phone" : payload.details.phone,
-        "nonce" : payload.nonce,
-        "payment_type" : payload.type,
-        "address_attributes" : {
-          "first_name" : first_name,
-          "last_name" : last_name,
-          "address_line_1" : payload.details.shippingAddress.line1,
-          "address_line_2" : payload.details.shippingAddress.line2,
-          "city" : payload.details.shippingAddress.city,
-          "state_code" : payload.details.shippingAddress.state,
-          "zip" : payload.details.shippingAddress.postalCode,
-          "country_code" : payload.details.shippingAddress.countryCode
-        }
-      }
-    }
+    params = transactionParams(payload);
 
     Spree.ajax({
       url: Spree.pathFor("solidus_paypal_braintree/transactions"),
       type: 'POST',
       dataType: 'json',
-      data: transactionParams,
+      data: params,
       success: function(response) {
-        window.location.href = Spree.pathFor("checkout/confirm");
+        window.location.href = response.redirectUrl;
       },
       error: function(xhr) {
         console.error("Error submitting transaction")
       },
     });
+  }
+};
+
+addressParams = function(payload) {
+  if (payload.details.shippingAddress.recipientName) {
+    var first_name = payload.details.shippingAddress.recipientName.split(" ")[0];
+    var last_name = payload.details.shippingAddress.recipientName.split(" ")[1];
+  }
+  if (first_name == null || last_name == null) {
+    var first_name = payload.details.firstName;
+    var last_name = payload.details.lastName;
+  }
+
+  return {
+    "first_name" : first_name,
+    "last_name" : last_name,
+    "address_line_1" : payload.details.shippingAddress.line1,
+    "address_line_2" : payload.details.shippingAddress.line2,
+    "city" : payload.details.shippingAddress.city,
+    "state_code" : payload.details.shippingAddress.state,
+    "zip" : payload.details.shippingAddress.postalCode,
+    "country_code" : payload.details.shippingAddress.countryCode
+  }
+};
+
+transactionParams = function(payload) {
+  return {
+    "payment_method_id" : window.paymentMethodId,
+    "transaction" : {
+      "email" : payload.details.email,
+      "phone" : payload.details.phone,
+      "nonce" : payload.nonce,
+      "payment_type" : payload.type,
+      "address_attributes" : addressParams(payload)
+    }
   }
 };
 
