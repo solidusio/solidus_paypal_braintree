d948ebe3a2a7ad135644e5b1a13fae0247d6eee3
Dennis Marchand <dennis@thenumber.ca>
2017-03-15 15:05:33 -0700

    Add transaction_by_id method to braintree adapter
    There is plans for a view to display the details of a transaction.
    This is a method for getting a specific transaction.

--- app/helpers/solidus_paypal_braintree/braintree_adapter.rb
+++ app/helpers/solidus_paypal_braintree/braintree_adapter.rb
@@ -3,5 +3,9 @@ module SolidusPaypalBraintree
     def self.all_transactions
       Braintree::Transaction.search
     end
+
+    def self.transaction_by_id(id)
+      Braintree::Transaction.search { |search| search.id.is id }
+    end
   end
 end
--- spec/helpers/solidus_paypal_braintree/braintree_adapter_spec.rb
+++ spec/helpers/solidus_paypal_braintree/braintree_adapter_spec.rb
@@ -12,4 +12,13 @@ describe SolidusPaypalBraintree::BraintreeAdapter do
       expect(all_transactions).to be_a(Braintree::ResourceCollection)
     end
   end
+
+  describe '.transaction_by_id', vcr: {
+    cassette_name: 'braintree_adapter/transaction_by_id'
+  } do
+    let(:transaction) { subject.transaction_by_id('ah3fg1f3') }
+    it 'returns a braintree resource collection' do
+      expect(transaction).to be_a(Braintree::ResourceCollection)
+    end
+  end
 end
