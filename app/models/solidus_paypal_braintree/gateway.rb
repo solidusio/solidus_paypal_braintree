63e7b23938085415734bf425c93c98ac703c2735
Isaac Freeman <isaac@resolvedigital.co.nz>
2017-11-14 13:27:47 +1300

    Set Braintree log level as a gateway preference
    The `braintree` gem provides a default logger that logs at the INFO level, which clutters logs a bit.
    
    This replace sit with a logger that uses the standard Rails log level

--- app/models/solidus_paypal_braintree/gateway.rb
+++ app/models/solidus_paypal_braintree/gateway.rb
@@ -53,7 +53,7 @@ module SolidusPaypalBraintree
         merchant_id: preferred_merchant_id,
         public_key: preferred_public_key,
         private_key: preferred_private_key,
-        logger: Braintree::Configuration.logger.clone
+        logger: logger
       }
     end
 
@@ -230,6 +230,12 @@ module SolidusPaypalBraintree
     end
 
     private
+
+    def logger
+      Braintree::Configuration.logger.clone.tap do |logger|
+        logger.level = Rails.logger.level
+      end
+    end
 
     def dollars(cents)
       Money.new(cents).dollars
