a2b3d3213e27750d3059a6bac4f288c0db849a7b
Alessandro Lepore <lepore.ale@gmail.com>
2018-06-04 10:16:46 -0700

    Enlarge headless chrome windows size
    Make sure that all elements are always visible.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,7 +41,7 @@ end
 
 Capybara.register_driver(:headless_chrome) do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome \
-    chromeOptions: { args: %w[headless disable-gpu] }
+    chromeOptions: { args: %w[headless disable-gpu window-size=1600,1024] }
   capybara_options = {
     browser: :chrome,
     desired_capabilities: capabilities
