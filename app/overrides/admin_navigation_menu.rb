36a3dd15e462c77afa2366279edba8ac55989976
Jon Mason <jmason@juul.com>
2018-07-11 10:34:14 -0700

    Remove Deface
    Solidus no longer uses Deface, and the implicit dependency on it in this
    plugin broke our Solidus app. We decided to remove the use of Deface
    rather than make it a dependency, following Solidus' lead.

--- app/overrides/admin_navigation_menu.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: "spree/admin/shared/_settings_sub_menu",
-  name: "solidus_paypal_braintree_admin_navigation_configuration",
-  insert_bottom: "[data-hook='admin_settings_sub_tabs']",
-  partial: "solidus_paypal_braintree/configurations/admin_tab"
-)
