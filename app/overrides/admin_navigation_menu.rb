2abe661bd1d7d73e172fa713a702e9dda3e1aa2e
jacobherrington <jherrington.personal@gmail.com>
2018-10-23 16:52:07 -0500

    Remove deface reliance

--- app/overrides/admin_navigation_menu.rb
+++ app/overrides/admin_navigation_menu.rb
@@ -1,6 +1,8 @@
-Deface::Override.new(
-  virtual_path: "spree/admin/shared/_settings_sub_menu",
-  name: "solidus_paypal_braintree_admin_navigation_configuration",
-  insert_bottom: "[data-hook='admin_settings_sub_tabs']",
-  partial: "solidus_paypal_braintree/configurations/admin_tab"
-)
+unless Spree::Backend::Config.respond_to?(:menu_items)
+  Deface::Override.new(
+    virtual_path: "spree/admin/shared/_settings_sub_menu",
+    name: "solidus_paypal_braintree_admin_navigation_configuration",
+    insert_bottom: "[data-hook='admin_settings_sub_tabs']",
+    partial: "solidus_paypal_braintree/configurations/admin_tab"
+  )
+end
--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,1 +1,12 @@
 Spree::Admin::PaymentsController.helper :braintree_admin
+
+if Spree::Backend::Config.respond_to?(:menu_items) && Rails.env != "test"
+  Spree::Backend::Config.configure do |config|
+    config.menu_items << config.class::MenuItem.new(
+      [:paypal_braintree],
+      'wrench',
+      label: 'Braintree',
+      url: "#{ActionController::Base.relative_url_root}/solidus_paypal_braintree/configurations/list"
+    )
+  end
+end
