3772ada6c8b9c409871b29fbad5b027c44c8683f
jacob <jherrington.personal@gmail.com>
2018-10-24 09:38:35 -0500

    Remove conditional

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,6 +1,6 @@
 Spree::Admin::PaymentsController.helper :braintree_admin
 
-if Spree::Backend::Config.respond_to?(:menu_items) && Rails.env != "test"
+if Spree::Backend::Config.respond_to?(:menu_items)
   Spree::Backend::Config.configure do |config|
     config.menu_items << config.class::MenuItem.new(
       [:paypal_braintree],
