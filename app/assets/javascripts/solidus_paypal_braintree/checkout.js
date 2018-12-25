58719191200e7c2401c36b47bf4f9df91d5673e2
Oscar <oskarromero3@gmail.com>
2018-12-26 00:55:43 +0100

    fix possible endless event.PreventDefault() loop

--- app/assets/javascripts/solidus_paypal_braintree/checkout.js
+++ app/assets/javascripts/solidus_paypal_braintree/checkout.js
@@ -48,6 +48,8 @@ $(function() {
               $paymentForm.submit();
             }
           });
+        } else {
+          $paymentForm[0].submit() //https://stackoverflow.com/a/22982816/5837307
         }
       }
     });
