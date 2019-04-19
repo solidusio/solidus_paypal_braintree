408bff4561749868715301a6f71396eeab3349f8
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-04-19 16:05:44 -0500

    Fix translation key

--- lib/solidus_paypal_braintree/engine.rb
+++ lib/solidus_paypal_braintree/engine.rb
@@ -54,6 +54,7 @@ module SolidusPaypalBraintree
           config.menu_items << config.class::MenuItem.new(
             [:braintree],
             'cc-paypal',
+            label: 'braintree',
             url: '/solidus_paypal_braintree/configurations/list',
             condition: -> { can?(:list, SolidusPaypalBraintree::Configuration) }
           )
