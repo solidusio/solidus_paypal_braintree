57548f5832e061b228b704b0d191c5e835020bce
Maurizio De Santis <desantis.maurizio@gmail.com>
2019-06-14 18:54:41 +0200

    Point Source#name to #payment_type

--- spec/requests/spree/api/orders_controller_spec.rb
+++ spec/requests/spree/api/orders_controller_spec.rb
@@ -1,25 +1,39 @@
 require 'spec_helper'
 
 describe Spree::Api::OrdersController, type: :request do
-  describe 'GET show' do
+
+  vcr_attrs =
+    if Gem::Requirement.new('>= 2.6').satisfied_by?(SolidusSupport.solidus_gem_version)
+      {}
+    else
+      { vcr: { cassette_name: 'api/orders/show' } }
+    end
+
+  describe 'GET show', **vcr_attrs do
     context 'can render the payment source view for each gateway' do
       include_context 'order ready for payment'
 
+      let(:payment_source) do
+        SolidusPaypalBraintree::Source.create!(
+          payment_method: gateway,
+          payment_type: SolidusPaypalBraintree::Source::CREDIT_CARD,
+          user: order.user,
+          nonce: 'fake-nonce',
+          token: 'abcdef123456'
+        )
+      end
+
       before do
+        # allow_any_instance_of(SolidusPaypalBraintree::Source).to receive(:expiration_month).and_return(nil)
         @current_api_user = order.user
         stub_authentication!
         order.payments.create!(
           payment_method: gateway,
           amount: order.total,
           state: 'completed',
-          source: SolidusPaypalBraintree::Source.create!(
-            payment_method: gateway,
-            payment_type: SolidusPaypalBraintree::Source::CREDIT_CARD,
-            user: order.user,
-            nonce: 'fake-nonce',
-            token: 'abcdef123456'
-          )
+          source: payment_source
         )
+        # expiration_month
         order.next!
         get spree.api_order_path(order, token: user.spree_api_key)
       end
