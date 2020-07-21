d1e69e0dd77530a8cd2276ec31bf656ae967d948
Matthew Bass <pelargir@gmail.com>
2020-07-21 11:09:37 -0400

    Move Braintree initializer into to_prepare block
    This fixes a loading error that can occur when Rails controller caching
    is disabled in development mode.

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,8 +1,10 @@
-if SolidusSupport.backend_available?
-  Spree::Admin::PaymentsController.helper :braintree_admin
-end
+Rails.application.config.to_prepare do
+  if SolidusSupport.backend_available?
+    Spree::Admin::PaymentsController.helper :braintree_admin
+  end
 
-if SolidusSupport.frontend_available?
-  Spree::CheckoutController.helper :braintree_checkout
-  Spree::OrdersController.helper :braintree_checkout
+  if SolidusSupport.frontend_available?
+    Spree::CheckoutController.helper :braintree_checkout
+    Spree::OrdersController.helper :braintree_checkout
+  end
 end
